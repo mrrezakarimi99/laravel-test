d7d190295641af7195462ef8e2a18c572b73d196
reza <mr.rezakarimi99@gmail.com>
2021-11-24 16:56:04 +0330

    Fix route

--- Web/modules/Policy/Routes/api.php
+++ Web/modules/Policy/Routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (){
     Route::get('/policies' ,'V1\PolicyController@index');
-    Route::delete('/policies' ,'V1\PolicyController@delete');
+    Route::post('/policies' ,'V1\PolicyController@store');
+    Route::get('/policies/{policy}/trash' ,'V1\PolicyController@trash');
+    Route::delete('/policies/{policy}' ,'V1\PolicyController@delete');
 });
